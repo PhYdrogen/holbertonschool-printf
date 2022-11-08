65f9ebf2bbf0b00ff40c1169f777b18cb73732c7
EL Yamine BACO <baco@31-0-236.dynamic.csail.mit.edu>
2022-11-08 14:36:58 +0100

    AP

---
+++ main.c
@@ -0,0 +1,1 @@
+int _printf(const char *format, ...);
--- main.h
+++ main.h
@@ -1,8 +1,1 @@
-#ifndef MAIN_HEADER
-#define MAIN_HERDER
-#include <stdlib.h>
-#include <stdio.h>
 
-int _printf(const char *format, ...);
-
-#endif
