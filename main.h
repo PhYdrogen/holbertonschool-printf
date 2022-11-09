faac5462cb536e1c315ed253380b8f23285eea28
ElYamine974 <113912966+ElYamine974@users.noreply.github.com>
2022-11-09 14:12:20 +0100

    Update main.h

--- main.h
+++ main.h
@@ -17,5 +17,5 @@ int print_str(va_list i);
 int print_perc(va_list i);
 int verif_next_char(const char *format, va_list i, int *x);
 int print_int(va_list i);
-int print_double(va_liste i);
+int print_double(va_list i);
 #endif
